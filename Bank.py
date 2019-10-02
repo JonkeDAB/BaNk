532d3e8a0ae0dc5e7cc56f2183ef12eded5caf24
Jonas Wirth <jonas.wirth@learnet.se>
2019-10-02 13:37:43 +0200

    Gjort så att saldo sparar och skriver till en fil

--- Bank.py
+++ Bank.py
@@ -25,22 +25,23 @@ while inloggning == 0:
 
 
 # Insättning/ Uttag
-saldo = 0
+saldoFil = open("saldo.txt", "r+")
+saldo = float(saldoFil.readline())
 menu = 0
 while menu == 0:
     menu = int(input("Insättning [1], uttag [2], övrigt [3], avsluta [4]: "))
     if menu == 1: # Insättning
-        try:
-            insättning = float(input("Hur mycket vill du sätta in? "))
-        except:
-            menu = 1
+        insättning = float(input("Hur mycket vill du sätta in? "))
         saldo = saldo + insättning
+
+        saldoFil.write(str(saldo))
+        saldoFil.close()
         print(saldo)
         menu = 0
 
     elif menu == 2: # Uttag
         uttag = float(input("Hur mycket vill du ta ut? "))
-        saldo = saldo - uttag
+        balance = saldo = saldo - uttag
         if saldo < 0:
             print("Oops nu har du inga pengar kvar")
             menu = 0
